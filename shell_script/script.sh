51505dbf2d8e4e5cb8893504c6ca4f7fd1aac91d
isabeli-k <isabel@wordsinside.com>
2024-11-27 19:13:51 +0000

    Waves functions improved and shell script mended

--- shell_script/script.sh
+++ shell_script/script.sh
@@ -1,3 +1,29 @@
-g++ -c ~/Desktop/C/C++/main.cpp
-g++ ~/Desktop/C/C++/main.o -o sfml-app -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
-./sfml-app
+#!/bin/bash
+
+SOURCE_CPP="$HOME/Desktop/C/C++/theremin/sound.cpp"
+OBJECT_FILE="$HOME/Desktop/C/C++/theremin/sound.o"
+OUTPUT_FILE="sfml-app"
+
+COMPILER="g++"
+FLAGS="-Wall -Iinclude"
+LINK_FLAGS="-lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio"
+
+echo "Compiling $SOURCE_CPP..."
+$COMPILER -c $SOURCE_CPP -o $OBJECT_FILE $FLAGS
+
+if [ $? -ne 0 ]; then
+    echo "Compilation failed."
+    exit 1
+fi
+
+echo "Linking to create $OUTPUT_FILE..."
+$COMPILER $OBJECT_FILE -o $OUTPUT_FILE $LINK_FLAGS
+
+if [ $? -eq 0 ]; then
+    echo "Build successful. Running the program..."
+    ./$OUTPUT_FILE
+else
+    echo "Linking failed."
+    exit 1
+fi
+
--- sound.h
+++ sound.h
@@ -5,19 +5,18 @@
 
 namespace sound {
 	#define TWO_PI 6.283185307
-
+	
 	short SineWave (double time, double freq, double amp) {
-	  short result;
-	  double tpc = 44100 / freq; //ticks per cycle
-	  double cycles = time / tpc;
-	  double rad = TWO_PI * cycles;
-	  short amplitude = 32767 * amp;
-	  
-	  result = amplitude * sin(rad);
-
-	  return result;
+		// Calculate angular frequency
+		double omega = TWO_PI * freq / 44100;
+		
+		// Calculate amplitude
+		double amplitude = (short)(32767 * amp);
+		
+		// Result as sine of phase angle
+		return (short)(amplitude * sin(omega * time));
 	}
-
+	
 	short SquareWave (double time, double freq, double amp) {
 		short result = 0;
 		int tpc = 44100 / freq;
@@ -31,33 +30,31 @@ namespace sound {
 
 		return result;
 	}
-   
+	
 	short TriangleWave (double time, double freq, double amp) {
-		short result = 0;
-		int tpc = 44100 / freq;
-		int cyclepart = (int)time % tpc;
-		int halfcycle = tpc / 2;
-		short amplitude = 32767 * amp;
+		// Ticks per cycle and amplitude
+		double tpc = 44100 / freq;
+		double cyclepart = fmod(time, tpc);
+		short amplitude = (short)(32767 * amp);
 		
-		if (cyclepart < halfcycle) {
-			result = (short)((2.0 * cyclepart / halfcycle - 1.0) * amplitude);
-		} else {
-			result = (short)((-2.0 * cyclepart / halfcycle + 1.0) * amplitude);
-		}
+		// Triangle wave value -- normalized to [0, 2]
+		double normal = (cyclepart / tpc) * 2.0;
+		double wave = (normal <= 1.0) ? (2.0 * normal - 1.0) : (-2.0 * (normal - 1.0) + 1.0);
 		
-		return result;
+		return (short)(wave * amplitude);
 	}
 	
 	short SawtoothleWave (double time, double freq, double amp) {
-		short result = 0;
-		int tpc = 44100 / freq;
-		int cyclepart = (int)time % tpc;
-		short amplitude = 32767 * amp;
+		// Ticks per cycle and amplitude
+		double tpc = 44100 / freq;
+		double cyclepart = fmod(time, tpc);
+		short amplitude = (short)(32767 * amp);
 		
-		result = (short)((2.0 * cyclepart / tpc - 1.0) * amplitude);
+		// Calculate wave value
+		double wave = 2.0 * (cyclepart / tpc) - 1.0;
 		
-		return result;
+		return (short)(wave * amplitude);
 	}
+	
 }
-
 #endif
